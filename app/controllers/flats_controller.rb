9b0f5d5dfba2e0c01dff52e57c46372d31ccf221
Joao Paulo Rosa <joaopaulosrosa@gmail.com>
2021-10-13 23:07:45 -0300

    simple_form

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,20 @@
+class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+end
