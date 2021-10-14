a289b03acefa6c67361d805897c8ec586e76c9a7
Joao Paulo Rosa <joaopaulosrosa@gmail.com>
2021-10-14 12:21:03 -0300

    done with the functionality, now going to work with the layout

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,5 @@
 class FlatsController < ApplicationController
-  before_action :set_flat, only: [:show]
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
 
   def index
     @flats = Flat.all
@@ -12,9 +12,39 @@ class FlatsController < ApplicationController
     @flat = Flat.new
   end
 
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to @flat
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @flat.update(flat_params)
+      redirect_to @flat
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @flat.destroy
+
+    redirect_to root_path
+  end
+
   private
 
   def set_flat
     @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :number_of_guests, :price_per_night)
   end
 end
